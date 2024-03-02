8ea43db810721aaa243cd37c7a1ad4fbc3999f61
Jason Claeys <jdclaeys@outlook.com>
2024-03-02 01:19:38 -0600

    initial todos list layout

---
+++ scripts/todos.js
@@ -0,0 +1,19 @@
+const titleInput = document.querySelector('#list-title')
+const addTitleBtn = document.querySelector('#add-title-btn')
+const itemInput = document.querySelector('#item')
+const addTodoBtn = document.querySelector('#add-todo-btn')
+
+const renderList = () => {
+    // Add code here
+}
+
+const addListTitle = () => {
+    // Add code here
+}
+
+const addListItem = () => {
+    // Add code here
+}
+
+addTitleBtn.addEventListener('click', addListTitle)
+addTodoBtn.addEventListener('click', addListItem)
